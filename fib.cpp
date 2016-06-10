92bebfeaae22938fba618b0c896ea6ec1d1008d9
yafengabc <yafengabc@gmail.com>
2016-06-10 17:34:24 -0500

    Update fib.cpp

--- fib.cpp
+++ fib.cpp
@@ -1,16 +1,20 @@
 #include "iostream"
 #include "time.h"
-
 using namespace std;
-int fib(int n){
-	if(n==0)
-		return 0;
-	if(n==1)
-		return 1;
-	return fib(n-1)+fib(n-2);
-}
+
+class fibn{
+public:
+	int fib(int n){
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		return fib(n-1)+fib(n-2);
+	}
+};
 int main(){
 	clock_t t=clock();
-	cout<<fib(40)<<endl;
+	fibn f=fibn();
+	cout<<f.fib(40)<<endl;
 	cout<<(clock()-t)/1000.0/1000.0<<" sec"<<endl;
 }
