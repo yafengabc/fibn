f351899029605f6054f32f972a7fc279f98adca3
“kirigaya” <zhuzi@linuser.org>
2016-06-10 08:46:18 +0800

    cpp version update

--- fib.cpp
+++ fib.cpp
@@ -1,15 +1,20 @@
 #include "iostream"
 #include "time.h"
+using namespace std;
 
-static int fib(int n){
-	if(n==0)
-		return 0;
-	if(n==1)
-		return 1;
-	return fib(n-1)+fib(n-2);
-}
+class fibn{
+public:
+	int fib(int n){
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		return fib(n-1)+fib(n-2);
+	}
+};
 int main(){
 	clock_t t=clock();
-	printf("%d\n",fib(40));
-	printf("%f sec\n",(clock()-t)/1000.0/1000.0);
+	fibn f=fibn();
+	cout<<f.fib(40)<<endl;
+	cout<<(clock()-t)/1000.0/1000.0<<" sec"<<endl;
 }
